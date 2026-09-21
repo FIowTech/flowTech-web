34e139991c6308afd026171f829a180eca69de3f
Gabriel <gabriel.carrara@sptech.school>
2026-09-21 17:47:06 -0300

    feat: new institutional cards

---
+++ app/src/components/institucional/kpi.jsx
@@ -0,0 +1,11 @@
+export function Kpi ({titulo, porcentagem, status}) {
+return(
+      <div className="flex flex-col rounded-lg border border-gray-200 p-5 shadow-xl w-[23%]">
+        <span>{titulo}</span>
+        <p className="text-[0.75rem]">
+          <span className="text-[2rem] font-bold">{porcentagem}</span> STATUS:
+          <span>{status}</span>
+        </p>
+      </div>
+)
+}
