0eb784f5b8a9e254746f69a3c223867e13d97e52
arthurr-rosante <arrosante@gmail.com>
2026-09-23 21:08:53 -0300

    chore: add react-router and phosphor/icons as dependencies

--- app/src/components/institucional/navbar.jsx
+++ app/src/components/institucional/navbar.jsx
@@ -1,62 +1,54 @@
+import { PhoneIcon } from "@phosphor-icons/react";
+import { Link } from "react-router";
 
 export function Navbar() {
   const navitems = [
-    { text: "Nossa Missão" , href: "index.html#nossa_missao"},
-    { text: "Nossa Solução" , href: "index.html#nossa_solucao"},
-    { text: "Recursos" , href: "index.html#recuros"},
-    { text: "Contato" , href: "index.html#contato"},
+    {
+      text: "Nossa Missão",
+      href: "#nossa_missao",
+      icon: null,
+    },
+    { text: "Nossa Solução", href: "#nossa_solucao", icon: null },
+    { text: "Recursos", href: "#recuros", icon: null },
+    {
+      text: "Contato",
+      href: "#contato",
+      icon: <PhoneIcon size={24} weight="duotone" />,
+    },
   ];
   return (
-
     <header className="flex flex-col items-center gap-4 px-6 py-5 sm:flex-row sm:justify-between sm:px-8 lg:px-12">
-    <span className="flex items-center">
-      <h1 className="text-2xl font-bold">FlowTech</h1>
-    </span>
+      <span className="flex items-center">
+        <h1 className="text-2xl font-bold">FlowTech</h1>
+      </span>
 
-    <nav className="flex flex-wrap justify-center gap-1 text-sm sm:gap-3 lg:gap-5">
-      {navitems.map((item) => (
-        <NavItem key={item.href} href={item.href} text={item.text}/>
-      ))}
-    <a href="login-cadastro.html" className="bg-green-500 rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-gray-100 sm:px-4">
-      Entrar
-    </a>
-    </nav>
-  </header>
- 
-    
+      <nav className="flex flex-wrap justify-center gap-1 text-sm sm:gap-3 lg:gap-5">
+        {navitems.map((item) => (
+          <NavItem
+            key={item.href}
+            href={item.href}
+            text={item.text}
+            icon={item.icon}
+          />
+        ))}
+        <Link
+          to="/login"
+          className="bg-green-500 rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-gray-100 sm:px-4"
+        >
+          Entrar
+        </Link>
+      </nav>
+    </header>
   );
 }
 
-function NavItem({ href, text}){
-  return ( 
-    <a
-    href={href}
-    className="rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4">
-      {text}
-    </a>
-    
+function NavItem({ href, text, icon }) {
+  return (
+    <Link
+      to={href}
+      className="flex items-center gap-2 rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4"
+    >
+      <p>{text}</p> {icon}
+    </Link>
   );
 }
-
-
-
-/*<header class="flex flex-col items-center gap-4 px-6 py-5 sm:flex-row sm:justify-between sm:px-8 lg:px-12">
-    <span class="flex items-center">
-      <h1 class="text-2xl font-bold">FlowTech</h1>
-    </span>
-
-    <nav class="flex flex-wrap justify-center gap-1 text-sm sm:gap-3 lg:gap-5">
-      <a href="index.html#nossa_missao"
-        class="rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4">Nossa Missão</a>
-      <a href="index.html#produto"
-        class="rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4">Nossa Solucão</a>
-      <a href="index.html#recursos"
-        class="rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4">Recursos</a>
-      <a href="index.html#secao_cadastro"
-        class="rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-green-500 sm:px-4">Contato</a>
-      <a href="login-cadastro.html"
-        class="bg-green-500 rounded px-3 py-2 transition duration-150 ease-in-out hover:bg-gray-100 sm:px-4">Entrar</a>
-    </nav>
-  </header>*/
-
-/*NavItem parametros*/
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.jsx";
+import "./index.css";
+import LoginPage from "./pages/loginpage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
+
+/* Exemplo de uso React-Router
+docs: https://reactrouter.com/start/declarative/routing
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+      // Rota index
+        <Route index element={<App />} />
+
+      // Layout - p/ compartilhar UI entre páginas distintas
+        <Route element={<AuthLayout/>}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
+
+function AuthLayout() {
+  return (
+    <div>
+      //... (qualquer estrutura JSX)
+      <Outlet/> // Outlet - serve para renderizar a UI dinamicamente com base na ROTA
+    </div>
+  )
+}
+
+*/
